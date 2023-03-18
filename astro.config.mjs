afe83e664ba2adc98e482240090e213179f54c54
Stuard Gerardo Carrillo Gonzalez <stuard@supergroup.co>
2023-03-18 14:44:09 -0600

    improve colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.stuardcg.dev",
+  site: "https://stuardcg.dev",
   integrations: [tailwind(), sitemap()],
 });
